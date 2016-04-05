7921aea2bec4c1304041f699b3528a4b435c19da
Toni Esteves <toni.esteves@gmail.com>
2016-04-05 17:04:49 -0300

    Configuração dos testes

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,7 +1,3 @@
 require 'rails_helper'
 
-describe Client do
-	it { should validate_presence_of(:register_number) }
-	it { should validate_presence_of(:name) }
-	it { should validate_presence_of(:status) }
 end
